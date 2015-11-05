997552f91723dfac61402d22f89fcd78455fc8d3
Carlos <carlinhoswake@gmail.com>
2015-11-05 11:43:47 -0200

    Melhorias no Teste Automatizado de Reservas

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -6,23 +6,7 @@ import org.junit.Test;
 public class SessaoTest {
 
 	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
-
-        Assert.assertTrue(sessao.podeReservar(1));
-	}
-	
-	@Test
-	public void deveVender5ingressosSeHa10vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(10);
-		
-		Assert.assertTrue(sessao.podeReservar(5));
-	}
-
-	@Test
-	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
+	public void naoDeveVenderSeAQuantidadeSolicitadaForMaiorQueADisponivel() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 
@@ -32,9 +16,9 @@ public class SessaoTest {
 	@Test
 	public void deveVenderAQuantidadeSolicitadaSeHouverIngressosDisponiveis() throws Exception {
 		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(100);
+		sessao.setTotalIngressos(2);
 
-		Assert.assertTrue(sessao.podeReservar(100));
+		Assert.assertTrue(sessao.podeReservar(2));
 	}
 	
 	@Test
