f387bfcc3e5fb6b50f659bbacbc0b3d02ef2e788
vckaqn <vck.aquino@gmail.com>
2015-11-06 09:29:13 -0200

    novos teste do Rodrigo Ferreira

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -141,4 +141,120 @@ public class EspetaculoTest {
 		return sessao;
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void naoDeveriaCriarSessoesDiariasQuandoDataInicioMaiorQueDataFim() {
+		//DADAS ESTAS ENTRADAS:
+		LocalDate hoje = new LocalDate();
+	  	LocalDate amanha = fim.plusDays(1);
+	  	LocalTime agora = new LocalTime();
+	  	Periodicidade diaria = Periodicidade.DIARIA;
+	  
+	  	//QUANDO EU DISPARAR O PROCESSAMENTO:
+	  	Espetaculo show = new Espetaculo();
+	  	List<Sessoes> sessoes = show.criarSessoes(amanha, hoje, agora, diaria);
+	  
+	  	//ESTAS SAO AS SAIDAS ESPERADAS:
+	  	//Nao tem asserts pq deveria jogar exception...
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void naoDeveriaCriarSessoesSemanaisQuandoDataInicioMaiorQueDataFim() {
+		//DADAS ESTAS ENTRADAS:
+		LocalDate hoje = new LocalDate();
+	  	LocalDate amanha = fim.plusDays(1);
+	  	LocalTime agora = new LocalTime();
+	  	Periodicidade semanal = Periodicidade.SEMANAL;
+	  
+	  	//QUANDO EU DISPARAR O PROCESSAMENTO:
+	  	Espetaculo show = new Espetaculo();
+	  	List<Sessoes> sessoes = show.criarSessoes(amanha, hoje, agora, semanal);
+	  
+	  	//ESTAS SAO AS SAIDAS ESPERADAS:
+	  	//Nao tem asserts pq deveria jogar exception...
+	}
+
+	@Test
+	public void deveriaCriarApenasUmaSessaoParaPeriodicidadeDiariaComDataInicioIgualDataFim() {
+		//DADAS ESTAS ENTRADAS:
+		LocalDate hoje = new LocalDate();
+	  	LocalTime agora = new LocalTime();
+	  	Periodicidade diaria = Periodicidade.DIARIA;
+	  
+	  	//QUANDO EU DISPARAR O PROCESSAMENTO:
+	  	Espetaculo show = new Espetaculo();
+	  	List<Sessoes> sessoes = show.criarSessoes(hoje, hoje, agora, diaria);
+	  
+	  	//ESTAS SAO AS SAIDAS ESPERADAS:
+		Assert.assertEquals(1, sessoes.size());
+	  
+	  	//Nao basta apenas verificar o size da lista, precisa garantir que criou as sessoes corretamente:
+	  	Sessao unica = sessoes.get(0);
+	  	Assert.assertEquals(show, unica.getEspetaculo());
+	    Assert.assertEquals(hoje.toDateTime(agora), unica.getInicio());
+	}
+
+	@Test
+	public void deveriaCriarApenasUmaSessaoParaPeriodicidadeSemanalComDataInicioIgualDataFim() {
+		//DADAS ESTAS ENTRADAS:
+		LocalDate hoje = new LocalDate();
+	  	LocalTime agora = new LocalTime();
+	  	Periodicidade semanal = Periodicidade.SEMANAL;
+	  
+	  	//QUANDO EU DISPARAR O PROCESSAMENTO:
+	  	Espetaculo show = new Espetaculo();
+	  	List<Sessoes> sessoes = show.criarSessoes(hoje, hoje, agora, semanal);
+	  
+	  	//ESTAS SAO AS SAIDAS ESPERADAS:
+		Assert.assertEquals(1, sessoes.size());
+	  
+	  	//Nao basta apenas verificar o size da lista, precisa garantir que criou as sessoes corretamente:
+	  	Sessao unica = sessoes.get(0);
+	  	Assert.assertEquals(show, unica.getEspetaculo());
+	    Assert.assertEquals(hoje.toDateTime(agora), unica.getInicio());
+	}
+
+	@Test
+	public void deveriaCriarCincoSessoesParaPeriodicidadeDiariaComIntervaloDeCincoDias() {
+		//DADAS ESTAS ENTRADAS:
+		LocalDate hoje = new LocalDate();
+	  	LocalDate daquiQuatroDias = hoje.plusDays(4);
+	  	LocalTime agora = new LocalTime();
+	  	Periodicidade diaria = Periodicidade.DIARIA;
+	  
+	  	//QUANDO EU DISPARAR O PROCESSAMENTO:
+	  	Espetaculo show = new Espetaculo();
+	  	List<Sessoes> sessoes = show.criarSessoes(hoje, daquiQuatroDias, agora, diaria);
+	  
+	  	//ESTAS SAO AS SAIDAS ESPERADAS:
+		Assert.assertEquals(5, sessoes.size());
+	  
+	  	//Nao basta apenas verificar o size da lista, precisa garantir que criou as sessoes corretamente:
+		for(int i = 0; i <= sessoes.size(); i++) {
+			Assert.assertEquals(show, criada.getEspetaculo());
+			Assert.assertEquals(hoje.plusDays(i).toDateTime(agora), criada.getInicio());
+	    }
+	}
+
+	@Test
+	public void deveriaCriarCincoSessoesParaPeriodicidadeSemanalComIntervaloDeCincoSemanas() {
+		//DADAS ESTAS ENTRADAS:
+		LocalDate hoje = new LocalDate();
+	  	LocalDate daquiQuatroSemanas = hoje.plusWeeks(4);
+	  	LocalTime agora = new LocalTime();
+	  	Periodicidade diaria = Periodicidade.SEMANAL;
+	  
+	  	//QUANDO EU DISPARAR O PROCESSAMENTO:
+	  	Espetaculo show = new Espetaculo();
+	  	List<Sessoes> sessoes = show.criarSessoes(hoje, daquiQuatroSemanas, agora, semanal);
+	  
+	  	//ESTAS SAO AS SAIDAS ESPERADAS:
+		Assert.assertEquals(5, sessoes.size());
+	  
+	  	//Nao basta apenas verificar o size da lista, precisa garantir que criou as sessoes corretamente:
+		for(int i = 0; i <= sessoes.size(); i++) {
+			Assert.assertEquals(show, criada.getEspetaculo());
+			Assert.assertEquals(hoje.plusWeeks(i).toDateTime(agora), criada.getInicio());
+	    }
+	}
+	
 }
